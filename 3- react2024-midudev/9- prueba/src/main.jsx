cd0ee339dc8210200232c5741bcbd643201be10f
EliDev <eliasss.mb@gmail.com>
2024-09-24 21:41:32 -0300

    prueba

--- 3- react2024-midudev/9- prueba/src/main.jsx
+++ 3- react2024-midudev/9- prueba/src/main.jsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  /// asdasdasdasdasdasd
-  // asdasd
-  // XDDDDDDDDDDDDDDD
   <React.StrictMode>
     <App />
   </React.StrictMode>,
